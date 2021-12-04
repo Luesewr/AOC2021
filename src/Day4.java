38edbcfaf2b39c9c7c1b22bb78d388f73dc1ed96
Laurens de Swart <l.j.p.deswart@student.tudelft.nl>
2021-12-04 15:02:18 +0100

    Removed Duplicate Code and Nested For Loops Day 4

--- src/Day4.java
+++ src/Day4.java
@@ -10,27 +10,15 @@ public class Day4 {
         File file = new File("input4.txt");
         Scanner scanner = new Scanner(file);
         Scanner sc = new Scanner(System.in);
-        System.out.println("""
-                Please enter a mode:
-                1: Part 1
-                2: Part 2""");
-        switch (sc.nextInt()) {
-            case 1 -> System.out.println(part1(scanner));
-            case 2 -> System.out.println(part2(scanner));
-            default -> System.out.println("Invalid input, please rerun and fill in a valid index.");
-        }
-    }
-
-    private static int part1(Scanner scanner) {
         List<Integer> numbers = new ArrayList<>();
         Arrays.stream(scanner.nextLine().split(",")).toList().forEach(s -> numbers.add(Integer.parseInt(s)));
-        List<BingoCard> boards = new ArrayList<>();
         List<String> lines = new ArrayList<>();
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             if (line.length() > 1)
                 lines.add(line);
         }
+        List<BingoCard> boards = new ArrayList<>();
         int i = 0;
         for (int j = 0; j < lines.size() / 5; j++) {
             BingoCard bingoCard = new BingoCard();
@@ -39,60 +27,45 @@ public class Day4 {
             }
             boards.add(bingoCard);
         }
+        System.out.println("""
+                Please enter a mode:
+                1: Part 1
+                2: Part 2""");
+        switch (sc.nextInt()) {
+            case 1 -> System.out.println(part1(numbers, boards));
+            case 2 -> System.out.println(part2(numbers, boards));
+            default -> System.out.println("Invalid input, please rerun and fill in a valid index.");
+        }
+    }
+
+    private static int part1(List<Integer> numbers, List<BingoCard> boards) {
+
         for (Integer integer : numbers) {
             for (BingoCard bingoCard : boards) {
-                for (int l = 0; l < 5; l++) {
-                    for (int m = 0; m < 5; m++) {
-                        if (bingoCard.getInt(l, m) == integer) {
-                            bingoCard.setTruth(l, m);
-                            if (bingoCard.checkWin()) {
-                                bingoCard.setWinningScore(integer);
-                                System.out.println(integer + ", ");
-                                bingoCard.print();
-                                return bingoCard.getScore();
-                            }
-                        }
-                    }
+                if (bingoCard.checkNumber(integer)) {
+                    bingoCard.setWinningScore(integer);
+                    System.out.println(integer + ", ");
+                    bingoCard.print();
+                    return bingoCard.getScore();
                 }
             }
         }
         return 0;
     }
 
-    private static int part2(Scanner scanner) {
-        List<Integer> numbers = new ArrayList<>();
-        Arrays.stream(scanner.nextLine().split(",")).toList().forEach(s -> numbers.add(Integer.parseInt(s)));
-        List<BingoCard> boards = new ArrayList<>();
-        List<String> lines = new ArrayList<>();
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            if (line.length() > 1)
-                lines.add(line);
-        }
-        int i = 0;
-        for (int j = 0; j < lines.size() / 5; j++) {
-            BingoCard bingoCard = new BingoCard();
-            for (int k = 0; k < 5; k++, i++) {
-                bingoCard.setGridRow(k, Arrays.stream(lines.get(i).trim().split("[ ]+")).mapToInt(Integer::parseInt).toArray());
-            }
-            boards.add(bingoCard);
-        }
+    private static int part2(List<Integer> numbers, List<BingoCard> boards) {
         BingoCard lastWinner = new BingoCard();
         int winners = 0;
         for (Integer integer : numbers) {
             for (BingoCard bingoCard : boards) {
-                for (int l = 0; winners < boards.size() && l < 5; l++) {
-                    for (int m = 0; winners < boards.size() && m < 5; m++) {
-                        if (bingoCard.getInt(l, m) == integer) {
-                            boolean wonAlready = bingoCard.checkWin();
-                            bingoCard.setTruth(l, m);
-                            if (!wonAlready && bingoCard.checkWin()) {
-                                bingoCard.setWinningScore(integer);
-                                lastWinner = bingoCard;
-                                winners++;
-                            }
-                        }
-                    }
+                boolean wonAlready = bingoCard.checkWin();
+                if (!wonAlready && bingoCard.checkNumber(integer)) {
+                    bingoCard.setWinningScore(integer);
+                    lastWinner = bingoCard;
+                    winners++;
+                }
+                if (winners == boards.size()) {
+                    break;
                 }
             }
         }
@@ -109,6 +82,17 @@ public class Day4 {
         public BingoCard() {
             grid = new int[5][5];
             truthGrid = new boolean[5][5];
+        }
+
+        public boolean checkNumber(int integer) {
+            for (int l = 0; l < 5; l++) {
+                for (int m = 0; m < 5; m++) {
+                    if (grid[l][m] == integer) {
+                        setTruth(l, m);
+                    }
+                }
+            }
+            return checkWin();
         }
 
         private boolean checkWin() {
@@ -139,10 +123,6 @@ public class Day4 {
                 }
             }
             return total;
-        }
-
-        public int getInt(int i, int j) {
-            return grid[i][j];
         }
 
         public int getScore() {
